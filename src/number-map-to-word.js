b7d1e839622ecba39ef5ca39345d00e25fdb6671
wyytzf <wyytzf@163.com>
2018-07-23 14:59:01 +0800

    Update number-map-to-word.js

--- src/number-map-to-word.js
+++ src/number-map-to-word.js
@@ -1,15 +1,15 @@
 function numberMapToWord(collection) {
   //在这里写入代码
   // test
-  return collection.map(function (c) {
-    const a = parseInt(c / 26);
-    const b = c % 26;
-    if (a === 0) {
-      return String.fromCharCode(b + 96)
-    } else {
-      return String.fromCharCode(a + 96, b + 96)
-    }
-}
-                        }
+    return collection.map(function (c) {
+      const a = parseInt(c / 26);
+      const b = c % 26;
+      if (a === 0) {
+        return String.fromCharCode(b + 96)
+      } else {
+        return String.fromCharCode(a + 96, b + 96)
+      }
+  }
+                          }
 
 module.exports = numberMapToWord;
